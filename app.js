89c02a85b711682bec5d971ba7c4d8dced65eb11
jainrishi2001 <jainrishi2001@gmail.com>
2023-07-04 15:58:03 +0530

    Intital Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+const request = require("request");
+const https = require("https");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+
+// mongoose.connect("mongodb://localhost:27017/<DB Name>", {useNewUrlParser:true});
+
+
+app.get("/",function(req,res){
+    res.render("home");
+  });
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
---
+++ public/js/carousel.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      // PointerEvent: none,
+      responsive: [{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      }, {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3
+        }
+      }]
+    });
+  });
